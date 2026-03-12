0b48d43a673bc82a282b0334bf7dbb20c43b1c9e
PeterKovar <kova@htl-donaustadt.at>
2026-03-12 17:18:54 +0100

    Implement training method and sigmoid derivative
    Added training functionality and sigmoid derivative method.

--- neuronnet.cpp
+++ neuronnet.cpp
@@ -22,6 +22,9 @@ NeuronNet::~NeuronNet(){
 double NeuronNet::sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x) );
 }
+double NeuronNet::sigmoid_derivative(double x) {
+    return x * (1.0 - x);
+}
 
 double NeuronNet::forward(double *input) {
     // Setzen der Werte in den Input - Layer
@@ -35,10 +38,43 @@ double NeuronNet::forward(double *input) {
             for(int k=0; k< layers[i].numberInputs; ++k){
                sum += layers[i-1].neurons[k].output *
                        layers[i].neurons[j].weight;
-               sum += layers[i].neurons[j].bias;
             }
+            sum += layers[i].neurons[j].bias;
             layers[i].neurons[j].output = sigmoid(sum);
         }
     }
     return layers[numberLayers-1].neurons[0].output;
+}
+
+void NeuronNet::train(double *input, double *target, double learning_rate){
+    //Einmal durchrechnen
+    forward(input);
+    //Fehler fuer den OutputLayer berechnen
+    for(int i=0; i < layers[numberLayers-1].numberNeurons; ++i){
+        double error = target[i] - layers[numberLayers-1].neurons[i].output;
+        layers[numberLayers-1].neurons[i].delta =
+                error * sigmoid_derivative(layers[numberLayers-1].neurons[i].output);
+    }
+    // Fehler zurueckrechnen (Backpropagation)
+    for(int i = numberLayers-2; i > 0; --i){
+        for(int j=0; j<layers[i].numberNeurons;j++){
+            double error = 0.0;
+            for(int k=0; k< layers[i+1].numberNeurons; ++k) {
+                error += layers[i+1].neurons[k].delta * layers[i+1].neurons[k].weight;
+            }
+            layers[i].neurons[j].delta = error
+                    + sigmoid_derivative(layers[i].neurons[j].output);
+        }
+    }
+    //Gewichte und Bias anpassen
+    for(int i=1; i< numberLayers; ++i){
+        for(int j=0; j<layers[i].numberNeurons; ++j){
+            for (int k=0; k< layers[i].numberInputs; ++k){
+                layers[i].neurons[j].weight += learning_rate *
+                        layers[i].neurons[j].delta * layers[i-1].neurons[k].output;
+            }
+            layers[i].neurons[j].bias +=
+                    learning_rate * layers[i].neurons[j].delta;
+        }
+    }
 }
